c96b29621ef5c475c9f6cd2d6db7b358aeadd5b2
Paolo Gallina <pgallina@newrelic.com>
2019-12-19 13:02:36 +0100

    test CI travis and github actions

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,12 @@
 FROM golang:1.10 as builder
-COPY . /go/src/github.com/newrelic/nri-elasticsearch/
-RUN cd /go/src/github.com/newrelic/nri-elasticsearch && \
+COPY . /go/src/github.com/gallo-cedrone/nri-elasticsearch/
+RUN cd /go/src/github.com/gallo-cedrone/nri-elasticsearch && \
     make && \
     strip ./bin/nri-elasticsearch
 
 FROM newrelic/infrastructure:latest
 ENV NRIA_IS_FORWARD_ONLY true
 ENV NRIA_K8S_INTEGRATION true
-COPY --from=builder /go/src/github.com/newrelic/nri-elasticsearch/bin/nri-elasticsearch /nri-sidecar/newrelic-infra/newrelic-integrations/bin/nri-elasticsearch
-COPY --from=builder /go/src/github.com/newrelic/nri-elasticsearch/elasticsearch-definition.yml /nri-sidecar/newrelic-infra/newrelic-integrations/definition.yaml
+COPY --from=builder /go/src/github.com/gallo-cedrone/nri-elasticsearch/bin/nri-elasticsearch /nri-sidecar/newrelic-infra/newrelic-integrations/bin/nri-elasticsearch
+COPY --from=builder /go/src/github.com/gallo-cedrone/nri-elasticsearch/elasticsearch-definition.yml /nri-sidecar/newrelic-infra/newrelic-integrations/definition.yaml
 USER 1000
--- win_build.ps1
+++ win_build.ps1
@@ -3,6 +3,8 @@
         This script verifies, tests, builds and packages a New Relic Infrastructure Integration
 #>
 param (
+    # IntegrationName
+    [string]$integration=$(throw "-integration is required"),
     # Target architecture: amd64 (default) or 386
     [ValidateSet("amd64", "386")]
     [string]$arch="amd64",
@@ -13,27 +15,10 @@ param (
     [switch]$skipTests=$false
 )
 
-$integration = $(Split-Path -Leaf $PSScriptRoot)
 $integrationName = $integration.Replace("nri-", "")
 $executable = "nri-$integrationName.exe"
 
-# verifying version number format
-$v = $version.Split(".")
-
-if ($v.Length -ne 3) {
-    echo "-version must follow a numeric major.minor.patch semantic versioning schema (received: $version)"
-    exit -1
-}
-
-$wrong = $v | ? { (-Not [System.Int32]::TryParse($_, [ref]0)) -or ( $_.Length -eq 0) -or ([int]$_ -lt 0)} | % { 1 }
-if ($wrong.Length  -ne 0) {
-    echo "-version major, minor and patch must be valid positive integers (received: $version)"
-    exit -1
-}
-
-echo "--- Configuring version $version for artifacts"
-
-.\windows_set_version.ps1 -major $v[0] -minor $v[1] -patch $v[2]
+.\windows_set_version.ps1 -integration $integration -version
 
 echo "--- Checking dependencies"
 
@@ -45,14 +30,6 @@ if (-not $?)
     exit -1
 }
 
-echo "Checking MSBuild.exe..."
-$msBuild = (Get-ItemProperty hklm:\software\Microsoft\MSBuild\ToolsVersions\4.0).MSBuildToolsPath
-if ($msBuild.Length -eq 0) {
-    echo "Can't find MSBuild tool. .NET Framework 4.0.x must be installed"
-    exit -1
-}
-echo $msBuild
-
 $env:GOOS="windows"
 $env:GOARCH=$arch
 
@@ -62,14 +39,14 @@ $goFiles = go list ./...
 
 echo "--- Format check"
 
-$wrongFormat = go fmt $goFiles
+#$wrongFormat = go fmt $goFiles
 
-if ($wrongFormat -and ($wrongFormat.Length -gt 0))
-{
-    echo "ERROR: Wrong format for files:"
-    echo $wrongFormat
-    exit -1
-}
+#if ($wrongFormat -and ($wrongFormat.Length -gt 0))
+#{
+#    echo "ERROR: Wrong format for files:"
+#    echo $wrongFormat
+#    exit -1
+#}
 
 if (-Not $skipTests) {
     echo "--- Running tests"
@@ -83,22 +60,19 @@ if (-Not $skipTests) {
 }
 
 echo "--- Running Build"
-
 go build -v $goFiles
+echo "--- Build completed, checking status"
 if (-not $?)
 {
     echo "Failed building files"
     exit -1
 }
 
-echo "--- Collecting Go main files"
-
-$packages = go list -f "{{.ImportPath}} {{.Name}}" ./...  | ConvertFrom-String -PropertyNames Path, Name
-$mainPackage = $packages | ? { $_.Name -eq 'main' } | % { $_.Path }
-
+#echo "--- Collecting Go main files"
+#$packages = go list -f "{{.ImportPath}} {{.Name}}" ./...  | ConvertFrom-String -PropertyNames Path, Name
+#$mainPackage = $packages | ? { $_.Name -eq 'main' } | % { $_.Path }
 echo "generating $integrationName"
-go generate $mainPackage
-
+go generate "github.com/gallo-cedrone/nri-elasticsearch/src"#$mainPackage
 $fileName = ([io.fileinfo]$mainPackage).BaseName
 
 echo "creating $executable"
@@ -112,7 +86,7 @@ echo "--- Building Installer"
 
 Push-Location -Path "pkg\windows\nri-$arch-installer"
 $env:integration = $integration
-. $msBuild/MSBuild.exe nri-installer.wixproj
+msbuild nri-installer.wixproj
 
 if (-not $?)
 {
